d74220b472316d23469122915f478f0052abd9ca
bilenciaga <nghtrung1001@gmail.com>
2023-04-17 10:28:30 +0900

    update 04/17

--- client/src/pages/Products/Products.jsx
+++ client/src/pages/Products/Products.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
+import {useParams} from 'react-router-dom'
+import { useState } from 'react'
+import List from '../../components/List/List'
+import './Products.scss'
+
 
 const Products = () => {
+
+  const catId = parseInt(useParams().id)
+  const [maxPrice,setMaxPrice] = useState(1000)
+  const [sort,setSort] = useState(null)
+
   return (
     <div className='products'>
       <div className="products__left">
@@ -23,24 +33,24 @@ const Products = () => {
           <h2>Filter by price</h2>
           <div className="products__left-filterItem">
             <span>0</span>
-            <input type="range" min={0} max={1000} />
-            <span>1000</span>
+            <input type="range" min={0} max={1000} onChange={(e) => setMaxPrice(e.target.value)}/>
+            <span>{maxPrice}</span>
           </div>
         </div>
         <div className="products__left-filter">
           <h2>Sort by</h2>
           <div className="products__left-filterItem">
-            <input type="radio" id='asc' value='asc' name='price' />
+            <input type="radio" id='asc' value='asc' name='price' onChange={e => setSort('asc')}/>
             <label htmlFor="asc">Price (Lowest first)</label>
           </div>
           <div className="products__left-filterItem">
-            <input type="radio" id='desc' value='desc' name='price' />
+            <input type="radio" id='desc' value='desc' name='price' onChange={e => setSort('dsc')}/>
             <label htmlFor="desc">Price (Highest first)</label>
           </div>
         </div>
       </div>
       <div className="products__right">
-        
+        <List catId={catId} maxPrice={maxPrice} sort={sort}/>
       </div>
     </div>
   )
